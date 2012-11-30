6427054f01788ebd28a436e7e0b1036b835b9c71
Drew Young <drewyoung1@gmail.com>
2012-11-30 17:37:41 -0500

    Fix (DB, Row, Column, Cell)#history

--- demo/computed-column.js
+++ demo/computed-column.js
@@ -8,35 +8,39 @@
 // r.context.through = require('through');
 // r.context.es      = require('event-stream');
 
-var through = require('through');
-var demux   = require('demux');
+var through     = require('through');
+var demux       = require('demux');
+var scuttlebutt = require('scuttlebutt-stream');
 
-var DB = require('../');
+var test = require('../test');
 
-var db = new DB('people');
+var r    = require('../test/interact');
 
-demux(db.column('firstName').createReadStream(), db.column('lastName').createReadStream()).pipe(through(function(data) {
-	try {
-		data = JSON.parse(data);
-	} catch(e) {
-		return;
-	}
+r.context.db  = test.db1;
+r.context.row = test.row1;
+r.context.s   = test.s1;
 
-	if(Array.isArray(data)) {
-		var update = data[0];
+var db  = test.db1;
+var row = test.row1;
 
-		var row = db.row(update.row);
+// var DB = require('../');
 
-		this.queue(JSON.stringify([{
-			row: update.row,
-			value: row.get('firstName') + ' ' + row.get('lastName')
-		}, data[1], data[2]]) + '\n');
-	}
+// var db = new DB('people');
+
+demux(db.column('firstName').createReadStream(), db.column('lastName').createReadStream()).pipe(scuttlebutt(function(update) {
+	var row = db.row(update.row);
+
+	this.queue({
+		row: update.row,
+		value: row.get('firstName') + ' ' + row.get('lastName')
+	});
 })).pipe(db.column('fullName').createWriteStream());
 
-var row = db.add({
-	firstName: 'Drew',
-	lastName: 'Young'
-});
+// var row = db.add({
+// 	firstName: 'Drew',
+// 	lastName: 'Young'
+// });
 
-process.nextTick(function() { console.log(row.get()); });
+// process.nextTick(function() {
+// 	console.log(row.get());
+// });
